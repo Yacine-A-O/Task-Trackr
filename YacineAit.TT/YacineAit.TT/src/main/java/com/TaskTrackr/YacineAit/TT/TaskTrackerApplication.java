bfc6946a2c292933b4b6981bafa6122e4df692b1
Yacine Ait Oubelli <yacined712@gmail.com>
2025-11-27 09:31:58 +0000

    Add files via upload

---
+++ YacineAit.TT/YacineAit.TT/src/main/java/com/TaskTrackr/YacineAit/TT/TaskTrackerApplication.java
@@ -0,0 +1,15 @@
+package com.TaskTrackr.YacineAit.TT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TaskTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerApplication.class, args);
+	}
+
+}
